d5dc4a1e3c548b62e9e131bbec6935311f441b27
Masahiro Okubo <stmhamachiii@gmail.com>
2020-12-24 22:27:38 +0900

    aboutページの追加

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,37 @@
 import Link from "next/link";
 import Layout from '../components/Layout';
+import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
 
-const About = () => (
-    <Layout title="About">
-        <div>
-            <Link href="/">Home</Link>
-            <p>A Javascript programmer</p>
-            <img src="/static/javascript-logo.png" alt="Javascript" height="200px"/>
-        </div>
-    </Layout>
-);
-
-export default About;
+export default class About extends Component<any, any> {
+    // public state = {
+    //     user: null
+    // };
+    //
+    static async getInitialProps() {
+        const res = await fetch('https://api.github.com/users/hiyashikyuri');
+        const data = await res.json();
+        return { user: data };
+    }
+    
+    // componentDidMount() {
+    //     fetch('https://api.github.com/users/readbarger')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             this.setState(
+    //                 { user: data }
+    //             );
+    //         });
+    // }
+    
+    render() {
+        const { user } = this.props;
+        return (
+            <Layout title="About">
+                <p>{ user.name }</p>
+                <p>A Javascript programmer</p>
+                <img src={ user.avatar_url } alt="Ne" height="200px"/>
+            </Layout>
+        );
+    }
+}
